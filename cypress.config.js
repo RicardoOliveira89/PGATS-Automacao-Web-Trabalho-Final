a8971131d0509986bff259443815ff82bc2e0c92
Ricardo <paulo.oliveira@cwi.com.br>
2025-10-26 19:09:42 -0300

    :hammer: Fix the pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,3 @@
-// const { defineConfig } = require("cypress");
-
-// module.exports = defineConfig({
-//   e2e: {
-//     reporter: 'cypress-mochawesome-reporter',
-//     baseUrl: 'https://automationexercise.com',
-//     setupNodeEvents(on, config) {
-//       require('cypress-mochawesome-reporter/plugin')(on)
-//     },
-//   },
-// });
-
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
@@ -19,9 +7,8 @@ module.exports = defineConfig({
     reporterOptions: {
       reportDir: "mochawesome-report",
       overwrite: false,
-      html: false, // gera só JSONs agora
+      html: true,
       json: true
     },
   },
 });
-
